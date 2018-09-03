46454b3cc36645e3341f51c60e8cfde4d7bd18ba
Jacqueline Chenault <adigitalnative@gmail.com>
2018-09-03 01:25:33 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -14,7 +14,7 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  
+  all_contestants(data)
 end
 
 def count_contestants_by_hometown(data, hometown)
