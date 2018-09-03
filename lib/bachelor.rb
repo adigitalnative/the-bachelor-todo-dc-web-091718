1476fd2cb4bf4d8373e4b597f40c4b96ff8c7c37
Jacqueline Chenault <adigitalnative@gmail.com>
2018-09-03 01:19:15 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -6,7 +6,6 @@ def get_first_name_of_season_winner(data, season)
   contestants = data[season]
   contestants.each do |contestant|
     if contestant[:status] = "Winner"
-      binding.pry
       winner = contestant["name"]
     end
   end
